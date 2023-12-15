a61208557bb2ab12dac62adc1774a981dd41e081
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 20:50:59 +0900

    feat 전체 로또들의 당첨금 변수화 및 출력 구현

--- src/main/java/lotto/model/Lottos.java
+++ src/main/java/lotto/model/Lottos.java
@@ -2,39 +2,63 @@ package lotto.model;
 
 import lotto.constants.LottoConstants;
 import lotto.constants.PrintMessage;
+import lotto.constants.PrizeResult;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Lottos {
     private List<Lotto> lottos;
+    private List<Integer> prizeLotto;
+    private int bonusLotto;
     private int lottoCount;
+
+    private int [] prizeCount;
+
     public Lottos(int buyPrice) {
         lottoCount = buyPrice / LottoConstants.LOTTO_PRICE.getNum();
-
         settingLottos();
     }
 
     private void settingLottos() {
         lottos = new ArrayList<>();
         for (int i = 0; i < lottoCount; i++) {
-            lottos.add(new Lotto(getRandomLottoNum()));
+            lottos.add(new Lotto(NumberGenerator.createRandomNumber()));
         }
     }
 
-    private List<Integer> getRandomLottoNum() {
-        List<Integer> numbers = new ArrayList<>();
-        int lottoCount = LottoConstants.LOTTO_NUMBER_COUNT.getNum();
-        for (int i = 0; i < lottoCount; i++) {
-            int num;
-            do {
-                num = NumberGenerator.createRandomNumber();
-            }while(numbers.contains(num));
+    public void setPrizeLotto(List<Integer> prizeLotto, int bonusLotto) {
+        this.prizeLotto = prizeLotto;
+        this.bonusLotto = bonusLotto;
 
-            numbers.add(num);
+        checkPrizeResult();
+    }
+
+    private void checkPrizeResult() {
+        prizeCount = new int[PrizeResult.values().length];
+        for (int i = 0; i < lottoCount; i++) {
+            int result = lottos.get(i).checkPrize(prizeLotto, bonusLotto);
+            if(result >= 0)
+               prizeCount[result]++;
+        }
+    }
+
+    public double getAvgPrize() {
+        double sum = 0;
+        PrizeResult[] values = PrizeResult.values();
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i].getPrice() * prizeCount[i];
         }
 
-        return numbers;
+        return sum / (lottoCount * LottoConstants.LOTTO_PRICE.getNum()) * 100;
+    }
+
+    public String printPrize() {
+        String result = "";
+        PrizeResult[] values = PrizeResult.values();
+        for (int i = 0; i < values.length; i++) {
+            result += values[i].toString() + " - " + prizeCount[i] + "개\n";
+        }
+        return result;
     }
 
     @Override
