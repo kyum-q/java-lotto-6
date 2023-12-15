16f2794acf72f967ae155b05a898e46112d2bf6c
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 20:49:13 +0900

    feat 출력 메시지 추가

--- src/main/java/lotto/constants/PrintMessage.java
+++ src/main/java/lotto/constants/PrintMessage.java
@@ -1,7 +1,8 @@
 package lotto.constants;
 
 public enum PrintMessage {
-    PRINT_RANDOM_LOTTO_COUNT("개를 구매했습니다.");
+    PRINT_RANDOM_LOTTO_COUNT("개를 구매했습니다."),
+    PRINT_RESULT_PRIZE("당첨 통계\n---");
 
     private String message;
 
