7024b078b407983789830cabd96ace3c976cc0c2
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 20:48:41 +0900

    fix 랜덤 값 설정 함수 변경

--- src/main/java/lotto/model/NumberGenerator.java
+++ src/main/java/lotto/model/NumberGenerator.java
@@ -3,8 +3,11 @@ package lotto.model;
 import camp.nextstep.edu.missionutils.Randoms;
 import lotto.constants.LottoConstants;
 
+import java.util.List;
+
 public class NumberGenerator {
-    public static int createRandomNumber() {
-        return Randoms.pickNumberInRange(LottoConstants.LOTTO_MIN_NUMBER.getNum(), LottoConstants.LOTTO_MAX_NUMBER.getNum());
+    public static List<Integer> createRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(LottoConstants.LOTTO_MIN_NUMBER.getNum(), LottoConstants.LOTTO_MAX_NUMBER.getNum(),
+                LottoConstants.LOTTO_NUMBER_COUNT.getNum());
     }
 }
