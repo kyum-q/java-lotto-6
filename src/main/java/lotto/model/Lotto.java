edf7d9e309d0d1f5536aa134eb1a7927f663e0e8
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 20:50:25 +0900

    feat 로또별로 당첨 금액 체크 구현

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,9 +1,14 @@
 package lotto.model;
 
+import lotto.constants.PrizeResult;
+
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
+    private int prizeCount;
+    private boolean isBonusPrize;
+
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
@@ -14,6 +19,23 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public int checkPrize(List<Integer> prize, int bonus) {
+        prizeCount = numbers.stream().mapToInt(n -> containCheck(prize, n)).sum();
+        isBonusPrize = numbers.contains(bonus);
+
+        if(isBonusPrize) prizeCount++;
+
+        if(prizeCount == 6 && !isBonusPrize)
+            return 4;
+        return prizeCount - 3;
+    }
+
+    private int containCheck(List<Integer> prize, int n) {
+        if(prize.contains(n))
+            return 1;
+        return 0;
     }
 
     @Override
