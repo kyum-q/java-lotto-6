107650c7ddfe103ddd7ae057e0fb3c783dfe9e03
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 19:02:48 +0900

    feat 로또번호 입력 및 예외 처리 구현

---
+++ src/main/java/lotto/validator/LottoNumbersValidator.java
@@ -0,0 +1,78 @@
+package lotto.validator;
+
+import lotto.constants.ErrorMessage;
+import lotto.constants.LottoConstants;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoNumbersValidator {
+    private List<Integer> lottoNumbers;
+    private ErrorMessage errorMessage;
+    private int minLottoNum, maxLottoNum, lottoCount;
+
+    public LottoNumbersValidator() {
+        errorMessage = ErrorMessage.ERROR_LOTTO_NUMBERS;
+        minLottoNum = LottoConstants.LOTTO_MIN_NUMBER.getNum();
+        maxLottoNum = LottoConstants.LOTTO_MAX_NUMBER.getNum();
+        lottoCount = LottoConstants.LOTTO_NUMBER_COUNT.getNum();
+    }
+    public boolean valid(String s) {
+        return validNumbers(s) && validLessOrOverLottoCount()
+                && validLessMinOrOverMaxNumber() && validOverlapNumbers();
+    }
+
+    private boolean validNumbers(String s) {
+        try {
+            lottoNumbers = new ArrayList<>();
+            Arrays.stream(s.split(",", -1))
+                    .forEach(num -> lottoNumbers.add(Integer.parseInt(num)));
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validLessOrOverLottoCount() {
+        try {
+            if(lottoNumbers.size() != lottoCount)
+                throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validLessMinOrOverMaxNumber() {
+        try {
+            if(lottoNumbers.stream().anyMatch(n -> n < minLottoNum || n > maxLottoNum)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validOverlapNumbers() {
+        try {
+            if(lottoNumbers.stream().distinct().count() != lottoCount) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return errorMessage.toString();
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return Collections.unmodifiableList(lottoNumbers);
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,12 +3,15 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import lotto.constants.RequestMessage;
 import lotto.validator.BuyPriceValidator;
+import lotto.validator.LottoNumbersValidator;
+
+import java.util.List;
 
 public class InputView {
     public int requestBuyLottoPrice() {
         BuyPriceValidator validator = new BuyPriceValidator();
 
-        System.out.println(RequestMessage.REQUEST_BUY_PRICE.getMessage());
+        System.out.println(RequestMessage.REQUEST_BUY_PRICE);
         String input = Console.readLine();
 
         while(!validator.valid(input)) {
@@ -17,5 +20,19 @@ public class InputView {
         }
 
         return Integer.parseInt(input);
+    }
+
+    public List<Integer> requestLottoNumbers() {
+        LottoNumbersValidator validator = new LottoNumbersValidator();
+
+        System.out.println(RequestMessage.REQUEST_LOTTO_NUMBERS);
+        String input = Console.readLine();
+
+        while(!validator.valid(input)) {
+            System.out.println(validator);
+            input = Console.readLine();
+        }
+
+        return validator.getLottoNumbers();
     }
 }
