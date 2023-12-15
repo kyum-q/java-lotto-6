299fcd239fcfc6b8c256173a013e37eb40d2c1ba
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 19:01:06 +0900

    chorm toString으로 천단위마다 , 출력하게 구현

--- src/main/java/lotto/constants/LottoConstants.java
+++ src/main/java/lotto/constants/LottoConstants.java
@@ -1,7 +1,10 @@
 package lotto.constants;
 
 public enum LottoConstants {
-    LOTTO_PRICE(1_000);
+    LOTTO_PRICE(1_000),
+    LOTTO_MIN_NUMBER(1),
+    LOTTO_MAX_NUMBER(45),
+    LOTTO_NUMBER_COUNT(6);
 
     private int num;
     LottoConstants(int num) {
@@ -10,5 +13,10 @@ public enum LottoConstants {
 
     public int getNum() {
         return num;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,d", num);
     }
 }
